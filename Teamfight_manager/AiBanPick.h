de91fb7a220379b89b00fb75aa1689c3e0d4123b
정승혁 <rgyrgjrgk@naver.com>
2023-08-09 13:35:44 +0900

    AiBanPick 클래스 추가

---
+++ Teamfight_manager/AiBanPick.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+struct Champion
+{
+    int Id;
+    int Attack;
+    int Defense;
+    int Hp;
+    float WinRate;
+};
+
+class AiBanPick
+{
+private:
+
+    Champion highStatChampion;
+    Champion secondStatChampion;
+
+    std::vector<Champion> highStatChampions;
+    std::vector<Champion> champions;
+
+    int weight = 5;
+    int winRateweight = 0.2;
+
+    bool isAiMistake = false;
+
+public:
+    AiBanPick();
+    ~AiBanPick();
+
+    int AddStat(const Champion& champion);
+    void printStats(const std::vector<Champion>& champions);
+    Champion CompareHighStatChampion(const Champion& a, const Champion& b);
+    Champion CompareSecondStatChampion(const Champion& a, const Champion& b);
+
+    void isBan(const Champion& highStatChampion, bool isAiMistake);
+    void isPick(const Champion& highStatChampion, bool isAiMistake);
+
+};
+
